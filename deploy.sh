3d94ddfbbbb9a4571bd4d93158a4b9b3c29fa280
Ling Qiu <lqiu@manh.com>
2019-08-15 13:32:04 -0400

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,17 @@
-docker build -t qiuling21/multi-client ./client/Dockerfile ./client
-docker build -t qiuling21/multi-server ./server/Dockerfile ./server
-docker build -t qiuling21/multi-worker ./worker/Dokcerfile ./worker
+GIT_SHA=$(git rev-parse HEAD)
+docker build -t qiuling21/multi-client:latest -t qiuling21/multi-client:$GIT_SHA ./client/Dockerfile ./client
+docker build -t qiuling21/multi-server:latest -t qiuling21/multi-server:$GIT_SHA ./server/Dockerfile ./server
+docker build -t qiuling21/multi-worker:latest -t qiuling21/multi-worker:$GIT_SHA ./worker/Dokcerfile ./worker
+
 docker push qiuling21/multi-client
 docker push qiuling21/multi-server
 docker push qiuling21/multi-worker
+
+docker push qiuling21/multi-client:$GIT_SHA
+docker push qiuling21/multi-server:$GIT_SHA
+docker push qiuling21/multi-worker:$GIT_SHA
+
 kubectl apply -f k8s
-kubectl set image deployment/client-deployment client=qiuling21/multi-client
+kubectl set image deployment/client-deployment client=qiuling21/multi-client:$GIT_SHA
+kubectl set image deployment/server-deployment server=qiuling21/multi-server:$GIT_SHA
+kubectl set image deployment/worker-deployment worker=qiuling21/multi-worker:$GIT_SHA
